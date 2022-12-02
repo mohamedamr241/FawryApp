2f6d15a7ab2ad776ff15cee810702396b1c8555b
Hagarabdelhamed <83194017+Hagarabdelhamed@users.noreply.github.com>
2022-12-02 18:04:37 +1200

    boundry

--- FawryAppProject/src/javaApp/AdminSystemBoundry.java
+++ FawryAppProject/src/javaApp/AdminSystemBoundry.java
@@ -1,5 +1,37 @@
 package javaApp;
+import java.util.*;
+import java.util.Scanner;
 
 public class AdminSystemBoundry {
-
+	
+	public void Display()
+	{
+	Scanner scan = new Scanner(System.in);
+	System.out.println("[1] Add Discount:");
+	System.out.println("[2] Check List OF Refund Requests:");	
+	int option = scan.nextInt();
+	switch (option) {
+	case 1:
+		Map<String, Integer> map = new HashMap<String, Integer>() ;
+		{
+			{  
+		        map.put("OverAll discount", 10);
+		        map.put("Specific discount", 20);
+			}
+		}
+	
+		break;
+		
+	case 2:
+		//Check  list of refund <string , int > -> <user email,amount of refund>
+		break;
+	
+	}
+	
+	}
+	
+	
+	
+	
+	
 }
