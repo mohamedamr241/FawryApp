e2739cb84ada15a6e4d6341fed8f5bdd6b67c104
nermin <nerminhassan2820@gmail.com>
2022-12-03 19:47:22 +0200

    Edit Service.java

--- FawryAppProject/src/javaApp/Services.java
+++ FawryAppProject/src/javaApp/Services.java
@@ -1,13 +1,27 @@
 package javaApp;
 
+import java.util.ArrayList;
+
 public abstract class Services {
 	public Handler providerHandler;
-	public abstract ServiceProviders createServiceProvider(String type);
-	public abstract void pay();
-	public abstract void displayPaymentForm();
-	public abstract void displayserviceProvidersForm();
-	public ServiceProviders orderServiceProvider(String type) {
-		ServiceProviders serv = createServiceProvider(type);
+	Payment paymentMethod;
+	ArrayList<String> serviceProviders = new ArrayList<String>();
+	ArrayList<String> paymentMethods = new ArrayList<String>();
+	public Services()
+	{
+		setData();
+	}
+	public abstract ServiceProviders createServiceProvider(int num);
+	protected abstract void setData();
+	public void displayProviders() {
+		for(int i = 1; i <= (int)serviceProviders.size(); i++)
+		{
+			System.out.println("[" + i + "] " + serviceProviders.get(i - 1));
+		}
+	}
+//	public abstract void displayPaymentForm();
+	public ServiceProviders orderServiceProvider(int num) {
+		ServiceProviders serv = createServiceProvider(num);
 		return serv;
 	}
 }
