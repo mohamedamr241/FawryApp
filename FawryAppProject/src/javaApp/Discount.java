aa1b6e80fb6a2d4027a68df6808650192c406543
YomnaMohammed <yomnamohammed120@gmail.com>
2022-12-04 17:33:57 +0200

    Update Discount.java

--- FawryAppProject/src/javaApp/Discount.java
+++ FawryAppProject/src/javaApp/Discount.java
@@ -1,18 +1,5 @@
 package javaApp;
-import java.util.*;
+
 public class Discount {
 
-	Map<String, Integer> map = new HashMap<String, Integer>() ;
-	{
-		{  
-	        map.put("OverAll discount", 10);
-	        map.put("Specific discount", 20);
-		}
-	}
-
-	
-	
-	
-	
-	
 }
