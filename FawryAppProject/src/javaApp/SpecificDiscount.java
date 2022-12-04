69ce71e62785f75e30f849e2ee91e9196f7ac63a
nermin <nerminhassan2820@gmail.com>
2022-12-04 19:20:59 +0200

    Update SpecificDiscount.java

--- FawryAppProject/src/javaApp/SpecificDiscount.java
+++ FawryAppProject/src/javaApp/SpecificDiscount.java
@@ -2,14 +2,35 @@ package javaApp;
 import java.util.*;
 
 public class SpecificDiscount extends Discount {
-	Map<String, Integer> map = new HashMap<String, Integer>() ; //to store service and its applied discount
+	
+	Payment payment;
+	static String serviceTmp;
+	public SpecificDiscount(Payment payment)
+	{
+		this.payment = payment;
+	}
+	
+	public static boolean searchService(String service)
+	{
+		for(String s : serviceDiscount.keySet())
+		{
+			if(s.equals(service))
+			{
+				serviceTmp = service;
+				return true;
+			}
+		}
+		return false;
+	}
+	static Map<String, Integer> serviceDiscount = new HashMap<String, Integer>() ; //to store service and its applied discount
 	{
 		{  
-	        map.put("mobile service",20);
+	        map.put("Mobile recharge service",20);
 		}
 	}
 	public double pay(double price) {
-		return price;
+		int discount = serviceDiscount.get(serviceTmp);
+		return payment.pay(price) - (price *(discount/100));
 	}
 	
 }
